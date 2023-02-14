e19444c0b39c5c5ca6c683edd2dbe0bde70f616d
Wohsaka <wooad32@gmail.com>
2023-02-14 10:18:21 -0400

    items end point modified

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -1,8 +1,26 @@
 const db = require('../database/db')
 
 const getItems = async (req, res) => {
+  const { category, query } = req.query
+  let items
   try {
-    const items = await db.query('SELECT * FROM items')
+    if (category && !query) {
+      items = await db.query('SELECT * FROM items WHERE category = $1', [
+        category,
+      ])
+    } else if (!category && query) {
+      items = await db.query(
+        `SELECT * FROM items WHERE LOWER(product_name) LIKE '%' || $1 || '%'`,
+        [query.toLowerCase()]
+      )
+    } else if (category && query) {
+      items = await db.query(
+        `SELECT * FROM items WHERE category = $1 AND LOWER(product_name) LIKE '%' || $2 || '%'`,
+        [category, query.toLowerCase()]
+      )
+    } else {
+      items = await db.query('SELECT * FROM items')
+    }
     if (items.rows.length <= 0) {
       return res
         .status(400)
--- src/database/database.sql
+++ src/database/database.sql
@@ -6,10 +6,11 @@ product_id TEXT NOT NULL,
 product_img TEXT NOT NULL, 
 product_description TEXT NOT NULL);
 
-INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description) VALUES ('Dog Food Premium 8kg', 
-20.00, 
+INSERT INTO items (product_name, product_price, category, product_id, product_img, product_description) VALUES (
+'Cat Food', 
+100.00, 
 'Food', 
-'DF01', 
-'https://happydog_en_sb.cstatic.io/440x440/f/69110/480x480/c90b13149a/hd-vet-new-product-slider-packshots-sensible-11kg-300g.png',
+'CF01', 
+'https://m.media-amazon.com/images/I/61tJAIhYFYL._SX466_.jpg',
 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate aut nobis est officiis culpa sapiente? Illum suscipit, explicabo qui consectetur corrupti reprehenderit repellendus perspiciatis ab at dicta obcaecati facere excepturi? Dicta deserunt totam alias magnam. Optio numquam ut explicabo, provident repellat voluptatibus.'
 );
